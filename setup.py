5372ee216dee2132bfa5c2a5889799d3a66a37e5
jhsu98 <jhsu98@gmail.com>
2020-10-14 11:02:08 -0400

    updated version in setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ HERE = pathlib.Path(__file__).parent
 README = (HERE / "README.md").read_text()
 
 setup(name='dfa-wrapper',
-      version='0.0.1',
+      version='0.0.2',
       description='Zerion Dataflow Automation API Wrapper',
       long_description=README,
       long_description_content_type="text/markdown",
